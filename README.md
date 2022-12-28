48c6f5f98e0e62a9f46f4cdf20a75e85edd12f7d
Murali Elumalai <muralismail4u@gmail.com>
2022-12-29 00:06:23 +0530

    Readme edit

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ A Finance management app built for Android/IOS/Mac/Windows
 This is built with flutter and uses sqlite for storing data locally 
 <br>
 <br>
-[<img src="https://user-images.githubusercontent.com/30234624/209856575-d6d5333e-ae7f-4cb4-b3f6-26eb90315eb7.jpeg" width="250"/>](img1)
-[<img src="https://user-images.githubusercontent.com/30234624/209856677-123e8cb4-ac4b-477d-b84b-b873f58ccf42.jpeg" width="250"/>](img2)
-[<img src="https://user-images.githubusercontent.com/30234624/209856688-d760249c-a8a8-4dc6-ac38-d4515a9da043.jpeg" width="250"/>](img3)
+[<img src="https://user-images.githubusercontent.com/30234624/209856575-d6d5333e-ae7f-4cb4-b3f6-26eb90315eb7.jpeg" width="200"/>](img1)
+[<img src="https://user-images.githubusercontent.com/30234624/209856677-123e8cb4-ac4b-477d-b84b-b873f58ccf42.jpeg" width="200"/>](img2)
+[<img src="https://user-images.githubusercontent.com/30234624/209856688-d760249c-a8a8-4dc6-ac38-d4515a9da043.jpeg" width="200"/>](img3)
 
 I'm using this for my own personal use feel free to clone or add PR if needed
 
--- lib/pages/analyticspage.dart
+++ lib/pages/analyticspage.dart
@@ -1,3 +1,4 @@
+import 'package:finance_journal/data/sqlhelper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -10,6 +11,22 @@ class AnalyticsPage extends StatefulWidget {
 }
 
 class _AnalyticsPageState extends State<AnalyticsPage> {
+  List<Map<String, dynamic>> _spendings = [];
+
+  List<String> list = <String>['Regular', 'Two', 'Three', 'Four'];
+
+  String dropdownValue = 'Regular';
+
+  bool _isLoading = true;
+
+  void _refreshSpendings() async {
+    final data = await SQLHelper.getItems();
+    setState(() {
+      _spendings = data;
+      _isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +34,11 @@ class _AnalyticsPageState extends State<AnalyticsPage> {
         title: const Text('Analytics'),
         centerTitle: false,
       ),
-      body: Center(child: Text('Analytics')),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : const Center(child: Text('Analytics')),
     );
   }
 }
